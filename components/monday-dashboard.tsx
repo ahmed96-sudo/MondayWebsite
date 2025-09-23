2bf8281be0efc731dd02615a6a1d56d3cfbe9d75
ahmed96-sudo <asyd12855@gmail.com>
2025-09-23 12:34:28 +0300

    Modified the dropdown

--- components/monday-dashboard.tsx
+++ components/monday-dashboard.tsx
@@ -414,7 +414,7 @@ export function MondayDashboard() {
                     href="#"
                     className="block px-4 py-2 text-sm text-gray-300 hover:bg-gray-700 hover:text-white"
                   >
-                    New Table
+                    New Group of Tasks
                   </a>
                 </div>
               </div>
